cc60dda9f0b46a560012321b802dfc95fba55dd4
ptksp-zaky <ptksp.zaky@gmail.com>
2019-11-27 17:11:19 +0700

    fix header navigasi berubah bahasa

--- App/navigation/navigation.js
+++ App/navigation/navigation.js
@@ -1,34 +1,49 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {compose} from 'redux';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from '../Screen/Home/Home';
 import Calculator from '../Screen/CalculatorRedux/Calculator';
 import getLang from '../helper/language/MyLanguange';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-//List Route/Navigation
-const AppNavigator = createStackNavigator(
-  {
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        headerShown: false,
-      },
-    },
-    Calculator: {
-      screen: Calculator,
-      navigationOptions: {
-        headerTitle: getLang({id: 'Redux Calculator'}),
-        headerStyle: {
-          backgroundColor: '#3366FF',
+class Navigation extends Component {
+  render() {
+    //List Route/Navigation
+    const AppNavigator = createStackNavigator(
+      {
+        Home: {
+          screen: Home,
+          navigationOptions: {
+            headerShown: false,
+          },
+        },
+        Calculator: {
+          screen: Calculator,
+          navigationOptions: {
+            headerTitle: getLang({id: 'Redux Calculator'}),
+            headerStyle: {
+              backgroundColor: '#3366FF',
+            },
+          },
         },
       },
-    },
-  },
-  {
-    initialRouteName: 'Home',
-  },
-);
+      {
+        initialRouteName: 'Home',
+      },
+    );
 
-const Navigation = createAppContainer(AppNavigator);
+    const AppNavigation = createAppContainer(AppNavigator);
 
-export default Navigation;
+    return <AppNavigation />;
+  }
+}
+
+//export default Navigation;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  };
+};
+
+export default compose(connect(mapStateToProps))(Navigation);
